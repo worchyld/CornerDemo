e43c1ac7c5ea72b0ca046436f42229ef3055eceb
AD <silentflute.ad@gmail.com>
2018-09-06 15:35:57 +0100

    Add a scrollview

---
+++ CornerDemo/CornerDemo/Views/TypeBreakdownView.swift
@@ -0,0 +1,23 @@
+//
+//  TypeBreakdownView.swift
+//  CornerDemo
+//
+//  Created by Amarjit on 06/09/2018.
+//  Copyright © 2018 Amarjit. All rights reserved.
+//
+
+import UIKit
+
+class TypeBreakdownView: UIView {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
