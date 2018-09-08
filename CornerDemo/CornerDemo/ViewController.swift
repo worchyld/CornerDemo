b8e700a2a498f24fcc3cccd7b93856a272b4e9a6
AD <silentflute.ad@gmail.com>
2018-09-08 14:50:41 +0100

    Remove un-necessary code

--- CornerDemo/CornerDemo/ViewController.swift
+++ CornerDemo/CornerDemo/ViewController.swift
@@ -12,21 +12,14 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     weak var viewModel: RoundViewModel?
 
-    let mainPanelView: MainPanelView = {
-        return MainPanelView()
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setup()
+        setupView()
     }
 
-    private func setup() {
-        //self.view.backgroundColor = UIColor( Constants.Colours.bgColor )
-        view.addSubview(mainPanelView)
-        view.addConstraintsWithFormat(format: "H:|[v0]|", views: mainPanelView)
-        view.addConstraintsWithFormat(format: "V:|[v0(150)]", views: mainPanelView)
+    private func setupView() {
+        self.view.backgroundColor = UIColor( Constants.Colours.bgColor )
     }
 
     override func didReceiveMemoryWarning() {
