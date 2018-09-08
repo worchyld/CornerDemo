f5416fa1ddb64f4282b151b63db30e898efe510c
AD <silentflute.ad@gmail.com>
2018-09-08 06:00:00 +0100

    Update podfile.  Update storyboard.  Parse CSV

--- CornerDemo/CornerDemo/Models/RoundData.swift
+++ CornerDemo/CornerDemo/Models/RoundData.swift
@@ -7,6 +7,24 @@
 //
 
 import Foundation
+import CSV
+
+extension TimeInterval {
+    public static func parseDuration(_ timeString:String) -> TimeInterval {
+        guard !timeString.isEmpty else {
+            return 0
+        }
+
+        var interval:Double = 0
+
+        let parts = timeString.components(separatedBy: ":")
+        for (index, part) in parts.reversed().enumerated() {
+            interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
+        }
+
+        return interval
+    }
+}
 
 class RoundData {
     static var instance = RoundData()
@@ -15,5 +33,42 @@ class RoundData {
 
     init() {
         // pre-load rounds from CSV
+
+        guard let filepath = Bundle.main.path(forResource: "Round1", ofType: "csv") else {
+            print ("no filepath found")
+            return
+        }
+
+        let stream = InputStream(fileAtPath: filepath)!
+        let csv = try! CSVReader(stream: stream, hasHeaderRow: true, trimFields: true, delimiter: ",", whitespaces: NSCharacterSet.whitespaces)
+
+        var rounds: [Round] = [Round]()
+
+        while csv.next() != nil {
+
+            guard let ts = csv["ts"] else {
+                return
+            }
+            guard let str_punch_type_id = (csv["punch_type_id"]) else {
+                return
+            }
+            guard let str_speed_mph = (csv["speed_mph"]) else {
+                return
+            }
+            guard let str_power_g = (csv["power_g"]) else {
+                return
+            }
+
+            let tsInterval : TimeInterval = TimeInterval.parseDuration(ts)
+
+            let punch_type_id:Int = (Int(str_punch_type_id))!
+            let speed : Double = (Double(str_speed_mph))!
+            let power : Double = (Double(str_power_g))!
+
+            //  Add round
+            rounds.append( Round(ts: tsInterval, punch_type_id: punch_type_id, speed: speed, power: power) )
+        }
+
+        print (rounds.description)
     }
 }
--- CornerDemo/CornerDemo/ViewController.swift
+++ CornerDemo/CornerDemo/ViewController.swift
@@ -12,64 +12,20 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     weak var viewModel: RoundViewModel?
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var pageControl: UIPageControl!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let _ = RoundData()
 
         setup()
     }
 
     private func setup() {
-        self.scrollView.delegate = self
         self.view.backgroundColor = UIColor( Constants.Colours.bgColor )
-
-        let slides = setupSlides()
-        setupSlideView(slides: slides )
-        self.pageControl.numberOfPages = slides.count
-        self.pageControl.currentPage = 0
-        view.bringSubview(toFront: self.pageControl)
-
-        updateViewConstraints()
-    }
-
-    private func setupSlides() -> [Slide] {
-        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-
-        slide1.lblSlideTitle.text = "Slide 1"
-        slide2.lblSlideTitle.text = "Slide 2"
-
-        return [slide1, slide2]
-    }
-
-    private func setupSlideView(slides: [Slide]) {
-        let width = view.frame.width
-
-        self.scrollView.isPagingEnabled = true
-        self.scrollView.showsVerticalScrollIndicator = false
-        self.scrollView.alwaysBounceHorizontal = false
-
-        self.scrollView.frame = CGRect(x: 0, y: 325, width: width, height: 125)
-        self.scrollView.contentSize = CGSize(width: width * CGFloat(slides.count), height: 125)
-
-        let _ = slides.enumerated().map { (offset, element) in
-            element.frame = CGRect(x: width * CGFloat(offset), y: 0, width: width, height: 125)
-            scrollView.addSubview(element)
-        }
-
-        scrollView.updateConstraintsIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageIndex = round(scrollView.contentOffset.x / scrollView.frame.width)
-        pageControl.currentPage = Int(pageIndex)
-    }
 }
-
