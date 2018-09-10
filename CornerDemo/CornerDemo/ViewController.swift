c89ca6ffa97fcaff763c177a128aee08bdab87c5
AD <silentflute.ad@gmail.com>
2018-09-10 19:43:55 +0100

    Updating panel views

--- CornerDemo/CornerDemo/ViewController.swift
+++ CornerDemo/CornerDemo/ViewController.swift
@@ -15,10 +15,11 @@ private let reuseIdentifier = "CellId"
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate  {
 
     private lazy var graphView: GraphView = {
-        return GraphView.init(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 150))
+        let view = GraphView.init(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 150))
+        return view
     }()
     private lazy var panelCollectionView: PanelCollectionView = {
-        let view = PanelCollectionView.init(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height:155))
+        let view = PanelCollectionView.init(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height:150))
         return view
     }()
 
@@ -37,10 +38,20 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
 
     private func setupView() {
+        let top = self.navigationController?.navigationBar.frame.height ?? 44
         self.title = self.viewModel.pageTitle
         self.view.backgroundColor = UIColor( Constants.Colours.bgColor )
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        self.collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.collectionView.contentInset = UIEdgeInsets(top: top, left: 0, bottom: 0, right: 0)
+
+        let flow = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flow.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        let itemSpacing: CGFloat = 25
+        let itemsInOneLine: CGFloat = 1
+        let width = UIScreen.main.bounds.size.width - itemSpacing * CGFloat(itemsInOneLine - 1)
+        flow.itemSize = CGSize(width: floor(width/itemsInOneLine), height: 150)
+        flow.minimumInteritemSpacing = 25
+        flow.minimumLineSpacing = 25
 
         // Define the menus
          let menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as! UISideMenuNavigationController
--- CornerDemo/CornerDemo/Views/PanelCollectionView.swift
+++ CornerDemo/CornerDemo/Views/PanelCollectionView.swift
@@ -18,12 +18,21 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
 
     private lazy var flowLayout : UICollectionViewFlowLayout = {
+        /*
         let f = UICollectionViewFlowLayout()
         f.scrollDirection = .horizontal
         f.itemSize = CGSize(width: 165, height: 155)
         f.minimumInteritemSpacing = CGFloat.greatestFiniteMagnitude
         f.minimumLineSpacing = 5
-        return f
+         */
+
+        let flow = UICollectionViewFlowLayout()
+        flow.scrollDirection = .horizontal
+        flow.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        flow.itemSize = CGSize(width: 165, height: 155)
+        flow.minimumLineSpacing = 5
+
+        return flow
     }()
 
     private lazy var collectionView : UICollectionView = {
@@ -33,10 +42,36 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
         cv.dataSource = self
         cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         cv.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        cv.isPagingEnabled = false
-        cv.isScrollEnabled = false
+
+        cv.isScrollEnabled = true
+
         return cv
     }()
+
+    /*
+    private lazy var panels : [UIView] = {
+        let panelPunchView : PanelView = PanelView()
+        let panelSpeedView : PanelView = PanelView()
+        let panelPowerView : PanelView = PanelView()
+
+        let panels = [panelPunchView, panelSpeedView, panelPowerView]
+        return panels
+    }()
+    */
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layoutIfNeeded()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(collectionView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     // MARK: UICollectionViewDataSource
 
@@ -53,16 +88,10 @@ UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
 
         // Configure the cell
-        cell.backgroundColor = .red
+        let view = PanelView(frame: cell.frame)
+        cell.addSubview(view)
 
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.frame.width, height: self.frame.height)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
 }
